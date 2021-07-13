c4cfa0843fd86b30550c5f1a6f59ae49edf9ad67
Millie Schmidt <65649134+MillieSchmidt@users.noreply.github.com>
2021-07-13 18:44:13 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const progressBar = document.querySelector('#progress');
-
-document.onscroll = () => {
-    const distanceTop = window.pageYOffset;
-    const pageHeight = document.body.scrollHeight - window.innerHeight;
-
-    progressBar.style.width = (distanceTop / pageHeight) * 100 + '%' ;
-}
